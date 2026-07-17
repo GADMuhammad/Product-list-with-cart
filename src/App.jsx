51174c53862873f2215af664888580d31d9b6423
Muhammad Gad <muhammadgadwork@gmail.com>
2026-07-17 17:55:15 +0300

    Stale-State Bug's solved

--- src/App.jsx
+++ src/App.jsx
@@ -5,44 +5,50 @@ import Cart from "./components/Cart";
 export const UpdateProducts = createContext();
 
 function App() {
-  const allProducts = JSON.parse(localStorage.getItem("productsDetails")) || [];
-  const [products, setProducts] = useState(allProducts);
+  const [products, setProducts] = useState(
+    () => JSON.parse(localStorage.getItem("productsDetails")) || [],
+  );
 
   useEffect(() => {
     if (!localStorage.getItem("productsDetails")) {
       fetch("/data.json")
         .then((response) => response.json())
-        .then((data) => {
-          localStorage.setItem("productsDetails", JSON.stringify(data));
-          setProducts(data);
-        })
+        .then((data) => setProducts(data))
         .catch((error) => console.error("Error:", error)); // to handle errors
     }
   }, []);
 
-  const updateProducts = (action, index, number) => {
-    const updatedData = [...allProducts];
-
-    switch (action) {
-      case "change":
-        {
-          const theClickedProduct = updatedData[index]; // the product I wanna edit
-          updatedData[index] = {
-            ...theClickedProduct,
-            amountOfProductInCart: number
-              ? theClickedProduct.amountOfProductInCart + number
-              : 0,
-          };
-        }
-        break;
-      case "reset":
-        updatedData.forEach((product) => (product.amountOfProductInCart = 0));
-
-        break;
+  useEffect(() => {
+    if (products.length) {
+      localStorage.setItem("productsDetails", JSON.stringify(products));
     }
+  }, [products]);
 
-    localStorage.setItem("productsDetails", JSON.stringify(updatedData));
-    setProducts(updatedData);
+  const updateProducts = (action, index, number) => {
+    setProducts((prevProducts) => {
+      const updatedData = [...prevProducts];
+
+      switch (action) {
+        case "change":
+          {
+            const theClickedProduct = updatedData[index]; // the product I wanna edit
+            updatedData[index] = {
+              ...theClickedProduct,
+              amountOfProductInCart: number
+                ? theClickedProduct.amountOfProductInCart + number
+                : 0,
+            };
+          }
+          break;
+        case "reset":
+          updatedData.forEach(
+            (product) => (product.amountOfProductInCart = 0),
+          );
+          break;
+      }
+
+      return updatedData;
+    });
   };
 
   return (
