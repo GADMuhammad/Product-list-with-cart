1abdfcb86c21e86fec3cae790c49417318008bbb
Muhammad Gad <muhammadgadwork@gmail.com>
2026-07-17 18:40:20 +0300

    okay

--- src/components/SuccessModal.jsx
+++ src/components/SuccessModal.jsx
@@ -9,12 +9,11 @@ function SuccessModal({ productsInCart, handleCloseModal }) {
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 50 }}
       className="z-10 w-[42vw] select-none overflow-hidden rounded-xl bg-rose50 px-6 py-10 outline-none max-one:w-[55vw] max-two:w-[70vw] max-there:w-[90vw] max-md:px-4 max-sm:w-[98vw]"
-      open layout
+      open
+      layout
     >
       <img src="images/icon-order-confirmed.svg" alt="Order Confirmed Mark" />
-      <h2 className="pb-2 pt-5 text-3xl font-bold tracking-wide">
-        Order Confirmed
-      </h2>
+      <h2 className="pb-2 pt-5 text-3xl font-bold tracking-wide">Order Confirmed</h2>
       <p className="pb-6 text-gray">We hope you enjoy your food!</p>
 
       <motion.div
@@ -26,53 +25,35 @@ function SuccessModal({ productsInCart, handleCloseModal }) {
         animate="visible"
         className="mx-auto flex h-fit flex-col justify-between rounded-xl bg-rose100 px-5 py-4 max-md:px-2"
       >
-        {productsInCart.map(
-          ({ id, image, name, price, amountOfProductInCart }) => (
-            <motion.div
+        {productsInCart.map(({ id, image, name, price, amountOfProductInCart }) => (
+          <motion.div
+            variants={{
+              hidden: { opacity: 0, scale: 0.5 },
+              visible: { opacity: 1, scale: 1 },
+            }}
+            transition={{ type: "spring" }}
+            key={id}
+            className="border-b-solid grid grid-cols-6 grid-rows-2 border-b-2 border-b-rose300 py-2"
+          >
+            <motion.img
               variants={{
-                hidden: { opacity: 0, scale: 0.5 },
-                visible: { opacity: 1, scale: 1 },
+                hidden: { opacity: 0, scale: 0, rotate: -25 },
+                visible: { opacity: 1, scale: 1, rotate: 0 },
               }}
-              transition={{ type: "spring" }}
-              key={id}
-              className="border-b-solid grid grid-cols-6 grid-rows-2 border-b-2 border-b-rose300 py-2"
-            >
-              <motion.img
-                variants={{
-                  hidden: { opacity: 0, scale: 0, rotate: -25 },
-                  visible: { opacity: 1, scale: 1, rotate: 0 },
-                }}
-                transition={{ type: "spring", stiffness: 260, damping: 15 }}
-                src={image.thumbnail}
-                alt="image thumbnail"
-                className="row-span-2 h-16 w-16 rounded-xl object-cover"
-              />
-              <h3 className="max-sm: col-span-4 font-medium max-sm:self-center max-sm:justify-self-center">
-                {name}
-              </h3>
-              <span className="col-span-1 row-span-2 ml-auto self-center font-semibold tracking-wide text-rose900 max-sm:self-end">
-                ${(amountOfProductInCart * price).toFixed(2)}
-              </span>
-              <span className="font-semibold text-redMain max-sm:justify-self-center">
-                {amountOfProductInCart}x
-              </span>
-              <span className="text-gray max-sm:justify-self-center">
-                ${price.toFixed(2)}
-              </span>
-            </motion.div>
-          ),
-        )}
+              transition={{ type: "spring", stiffness: 260, damping: 15 }}
+              src={image.thumbnail}
+              alt="image thumbnail"
+              className="row-span-2 h-16 w-16 rounded-xl object-cover"
+            />
+            <h3 className="max-sm: col-span-4 font-medium max-sm:self-center max-sm:justify-self-center">{name}</h3>
+            <span className="col-span-1 row-span-2 ml-auto self-center font-semibold tracking-wide text-rose900 max-sm:self-end">${(amountOfProductInCart * price).toFixed(2)}</span>
+            <span className="font-semibold text-redMain max-sm:justify-self-center">{amountOfProductInCart}x</span>
+            <span className="text-gray max-sm:justify-self-center">${price.toFixed(2)}</span>
+          </motion.div>
+        ))}
         <div className="flex items-center justify-between pt-4">
           <h6 className="text-gray">Order Total</h6>
-          <h3 className="text-2xl font-bold tracking-wide">
-            $
-            {productsInCart
-              .reduce(
-                (acc, cur) => acc + cur.price * cur.amountOfProductInCart,
-                0,
-              )
-              .toFixed(2)}
-          </h3>
+          <h3 className="text-2xl font-bold tracking-wide">${productsInCart.reduce((acc, cur) => acc + cur.price * cur.amountOfProductInCart, 0).toFixed(2)}</h3>
         </div>
       </motion.div>
 
